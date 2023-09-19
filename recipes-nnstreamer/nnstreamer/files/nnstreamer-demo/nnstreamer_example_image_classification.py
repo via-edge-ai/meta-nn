fc2c6cd07c2a367a9ae6974fa96a38be3446add0
Suhrid Subramaniam <suhrid.subramaniam@mediatek.com>
2023-09-19 01:31:54 +0000

    nnstreamer: Collapse image classification demo
    Reduce duplication by merging the Image Classification demos into one file.
    
    Change-Id: Ic6f3183a58816e90b9ef5f26895189c755be345c
    Signed-off-by: Suhrid Subramaniam <suhrid.subramaniam@mediatek.com>

--- recipes-nnstreamer/nnstreamer/files/nnstreamer-demo/nnstreamer_example_image_classification.py
+++ recipes-nnstreamer/nnstreamer/files/nnstreamer-demo/nnstreamer_example_image_classification.py
@@ -33,12 +33,13 @@ class Demo:
     self.pipeline = None
     self.running = False
 
-    self.VIDEO_WIDTH = 640
-    self.VIDEO_HEIGHT = 480
+    self.VIDEO_WIDTH = 720
+    self.VIDEO_HEIGHT = 1280
     self.MODEL_INPUT_HEIGHT = 224
     self.MODEL_INPUT_WIDTH = 224
     self.FULLSCREEN = 0
     self.CAM_ID = 0
+    self.CAM_ROT = 0
 
     self.tflite_model = ''
     self.dla = ''
@@ -59,13 +60,25 @@ class Demo:
 
   def build_pipeline(self, engine):
     cmd = ''
-    cmd += f'v4l2src name=src device=/dev/video{self.CAM_ID} io-mode=mmap ! video/x-raw,width={self.VIDEO_WIDTH},height={self.VIDEO_HEIGHT},format=YUY2,framerate=30/1 ! tee name=t_raw '
+    if self.cam_type == 'uvc':
+      cmd += f'v4l2src name=src device=/dev/video{self.CAM_ID} io-mode=mmap ! video/x-raw,width={self.VIDEO_WIDTH},height={self.VIDEO_HEIGHT},format=YUY2,framerate=30/1 ! tee name=t_raw '
+    elif self.cam_type == 'yuvsensor':
+      cmd += f'v4l2src name=src device=/dev/video{self.CAM_ID} ! video/x-raw,width=2316,height=1746,format=UYVY,framerate=30/1 ! tee name=t_raw '
+    elif self.cam_type == 'rawsensor':
+      cmd += f'v4l2src name=src device=/dev/video{self.CAM_ID} ! video/x-raw,width=2048,height=1536,format=YUY2,framerate=30/1 ! tee name=t_raw '
     cmd += f't_raw. ! queue leaky=2 max-size-buffers=10 ! '
+    if self.cam_type != 'uvc':
+      cmd += f'v4l2convert output-io-mode=dmabuf-import extra-controls="cid,rotate={self.CAM_ROT}" ! video/x-raw,width={self.VIDEO_WIDTH},height={self.VIDEO_HEIGHT},format=RGB16,pixel-aspect-ratio=1/1 ! '
     cmd += f'textoverlay name=label font-desc=Sans,18 valignment=top ! '
-    cmd += f'fpsdisplaysink name=sink text-overlay=false signal-fps-measurements=true sync=false video-sink="waylandsink sync=false qos=false fullscreen={self.FULLSCREEN}"'
-
+    if self.cam_type == 'uvc':
+      cmd += f'fpsdisplaysink name=sink text-overlay=false signal-fps-measurements=true sync=false video-sink="waylandsink sync=false qos=false fullscreen={self.FULLSCREEN}"'
+    else:
+      cmd += f'fpsdisplaysink name=sink text-overlay=false signal-fps-measurements=true sync=false video-sink="glimagesink sync=false qos=false" '
     cmd += f't_raw. ! queue leaky=2 max-size-buffers=2 ! '
-    cmd += f'videoconvert ! videoscale ! video/x-raw,width={self.MODEL_INPUT_WIDTH},height={self.MODEL_INPUT_HEIGHT},format=RGB ! '
+    if self.cam_type == 'uvc':
+      cmd += f'videoconvert ! videoscale ! video/x-raw,width={self.MODEL_INPUT_WIDTH},height={self.MODEL_INPUT_HEIGHT},format=RGB ! '
+    else:
+      cmd += f'v4l2convert output-io-mode=dmabuf-import capture-io-mode=mmap extra-controls="cid,rotate={self.CAM_ROT}" ! video/x-raw,width={self.MODEL_INPUT_WIDTH},height={self.MODEL_INPUT_HEIGHT},format=RGB,pixel-aspect-ratio=1/1 ! '
     cmd += f'tensor_converter ! '
 
     if engine == 'neuronsdk':
@@ -244,7 +257,7 @@ class Demo:
 
 if __name__ == '__main__':
   logging.basicConfig(level=logging.INFO)
-  args = argument_parser_init(False)
+  args = argument_parser_init()
 
   example = Demo(sys.argv[1:])
   example.CAM_ID = args.cam
@@ -252,6 +265,15 @@ if __name__ == '__main__':
   example.VIDEO_HEIGHT = args.height
   example.FULLSCREEN = args.fullscreen
   example.THROUGHPUT = args.throughput
+  example.CAM_ROT = args.rot
+  example.cam_type = args.cam_type
+
+  if example.cam_type == 'uvc':
+    example.VIDEO_WIDTH = 640
+    example.VIDEO_HEIGHT = 480
+  else:
+    example.VIDEO_WIDTH = 720
+    example.VIDEO_HEIGHT = 1280
 
   enable_performance(args.performance)
 
